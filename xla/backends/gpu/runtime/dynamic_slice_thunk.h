80464305eb9d0e16e407ad3f5cb0d40a5a246458
Shraiysh <svaishay@nvidia.com>
2025-01-23 00:52:28 -0800

    PR #20794: [gpu][ds-fusion] Add handling for offset module in ds-fusion thunk
    Imported from GitHub PR https://github.com/openxla/xla/pull/20794
    
    This patch adds support for offset modules in ds fusion thunk. It also moves the `ResourceRequests` structure from `gpu_executable.cc` to `gpu_executable.h` because a valid implementation of the abstract class `Thunk::ResourceRequests` is required for calling `Thunk::Prepare()`.
    
    This is split from #20332 as per request.
    Copybara import of the project:
    
    --
    a6e006227c670629e0eec3df1f5ef673b5059a22 by Shraiysh Vaishay <svaishay@nvidia.com>:
    
    [gpu][ds-fusion] Add handling for offset module in ds-fusion thunk
    
    This patch adds support for offset modules in ds fusion thunk. It also
    moves the `ResourceRequests` structure from `gpu_executable.cc` to
    `gpu_executable.h` because a valid implementation of the abstract class
    `Thunk::ResourceRequests` is required for calling `Thunk::Prepare()`.
    
    --
    48def7abe4d1df081856b028335687c5f253e767 by Shraiysh Vaishay <svaishay@nvidia.com>:
    
    Address comments and rebase
    
    --
    8027622f94209219042fc33d601962b53ad14f3e by Shraiysh Vaishay <svaishay@nvidia.com>:
    
    Address comments
    
    --
    c38586f5ae3022c2e10449dfc9eb8bd8d0ed232d by Shraiysh Vaishay <svaishay@nvidia.com>:
    
    Addressed comments.
    
    Merging this change closes #20794
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/20794 from shraiysh:ds_fusion_thunk_changes c38586f5ae3022c2e10449dfc9eb8bd8d0ed232d
    PiperOrigin-RevId: 718748248

--- xla/backends/gpu/runtime/dynamic_slice_thunk.h
+++ xla/backends/gpu/runtime/dynamic_slice_thunk.h
@@ -47,8 +47,44 @@ class DynamicSliceThunk : public Thunk {
  public:
   // Dynamic slice offset can be either: (1) a statically known constant value
   // or (2) a truly dynamic offset that is computed on device and have to be
-  // transferred to host.
-  using Offset = std::variant<int64_t, BufferAllocation::Slice>;
+  // transferred to host or (3) a temporary HloModule that computes the offset
+  // with a single induction variable as the input.
+  using Offset = std::variant<int64_t, BufferAllocation::Slice, HloModule*>;
+
+  struct OffsetAsFunctionOfIndvarModulesMetadata {
+    // These two modules help keep track of the induction variable. The module
+    // `indvar_init_` is a module with prototype `() -> integer[]`. It takes
+    // no input, and returns the initial value of the induction variable. The
+    // module `indvar_update_` is a module with prototype `(integer[]) ->
+    // integer[]`. It takes the current value of the induction variable, and
+    // returns the next value of the induction variable.
+    std::unique_ptr<HloModule> indvar_init_, indvar_update_;
+
+    // Extracted HloModules for computing dynamic offsets. The modules are
+    // not used here, this is solely for keeping the modules alive and maintain
+    // their ownership with the thunk while their raw pointers would be used
+    // during execution from the `offsets_` vector. These modules are with
+    // signature `(integer[]) -> integer[]`, where the input is the current
+    // value of the loop induction variable, and the output is the offset value
+    // for that iteration.
+    std::vector<std::unique_ptr<HloModule>> extracted_offset_modules;
+
+    OffsetAsFunctionOfIndvarModulesMetadata(
+        std::unique_ptr<HloModule> indvar_init,
+        std::unique_ptr<HloModule> indvar_update,
+        std::vector<std::unique_ptr<HloModule>> extracted_offset_modules)
+        : indvar_init_(std::move(indvar_init)),
+          indvar_update_(std::move(indvar_update)),
+          extracted_offset_modules(std::move(extracted_offset_modules)) {
+      CHECK(indvar_init_ != nullptr && indvar_update_ != nullptr);
+      CHECK(indvar_init_->entry_computation()->num_parameters() == 0)
+          << "Induction variable init module expected with signature `() -> "
+             "integer[]`.";
+      CHECK(indvar_update_->entry_computation()->num_parameters() == 1)
+          << "Induction variable update module expected with signature "
+             "`(integer[]) -> integer[]`.";
+    }
+  };
 
   DynamicSliceThunk(
       ThunkInfo thunk_info, std::unique_ptr<ThunkSequence> embedded_thunk,
@@ -57,8 +93,9 @@ class DynamicSliceThunk : public Thunk {
       std::vector<std::optional<std::vector<Offset>>> offsets,
       std::vector<std::optional<Shape>> orig_shapes,
       std::vector<std::optional<Shape>> sliced_shapes,
-      std::vector<std::optional<uint64_t>> offset_byte_sizes);
-
+      std::vector<std::optional<uint64_t>> offset_byte_sizes,
+      std::optional<OffsetAsFunctionOfIndvarModulesMetadata>
+          offset_as_function_of_indvar_metadata = std::nullopt);
   DynamicSliceThunk(const DynamicSliceThunk&) = delete;
   DynamicSliceThunk& operator=(const DynamicSliceThunk&) = delete;
 
@@ -132,6 +169,12 @@ class DynamicSliceThunk : public Thunk {
 
   // A mapping from argument index to the base offset in the `offsets_allocs_`.
   std::vector<int64_t> offsets_allocs_base_;
+
+  // This structure holds the metadata for offset computation on host. It stores
+  // a single induction variable initialization module, its update module and
+  // the offsets that are a function of the induction variable.
+  std::optional<OffsetAsFunctionOfIndvarModulesMetadata>
+      offset_as_function_of_indvar_metadata_;
 };
 
 }  // namespace gpu
